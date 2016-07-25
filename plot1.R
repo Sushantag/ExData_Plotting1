551e1ad97b96c2df5583ca077fff85c0f3abefad
Sushantag <sushant24@gmail.com>
2016-07-25 10:10:00 +0800

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,20 @@
+#Read just two lines to extract the header
+header <- read.table("household_power_consumption.txt", header = T, sep = ";", nrows = 2, na.strings = "?")
+
+#Read the lines relevant to 1-Feb-2007 to 2-Feb-2007
+hpc <- read.table("household_power_consumption.txt", header = F, sep = ";", skip = 66637, nrows = 2880, na.strings = "?")
+
+#assign header to hpc data frame
+names(hpc) <- names(header)
+
+#delete header data frame
+rm(header)
+
+#combine Date and Time Columns and convert to R date and time format
+hpc$Date_Time <- paste(hpc$Date, hpc$Time, sep = " ")
+hpc$Date_Time <- strptime(hpc$Date_Time, "%d/%m/%Y %H:%M:%S")
+
+#Plot 1
+png("plot1.png",width=480,height=480)
+hist(hpc$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
